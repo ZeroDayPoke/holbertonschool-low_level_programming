38e53cf36716b51622feff2ac1e07bd9bd74ccda
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 11:33:36 -0500

    betty compliance added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: 'betty compliant example'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-    char strArr[101];
-    strcpy(strArr, "Programming is like building a multilingual puzzle");
-    puts(strArr);
-    return (0);
+	char strArr[101];
+
+	strcpy(strArr, "Programming is like building a multilingual puzzle");
+	puts(strArr);
+	return (0);
 }
