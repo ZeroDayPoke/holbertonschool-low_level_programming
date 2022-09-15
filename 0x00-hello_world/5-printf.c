d5f0891a54a280d74e221e81f721da1b81e7a557
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 10:27:54 -0500

    push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char strArr[101];
+    strcpy(strArr, "with proper grammar, but the outcome is a piece of art,");
+    printf("%s", strArr);
+    return (0);
+}
