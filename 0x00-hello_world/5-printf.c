b0feae8a55a3a794b0f359397b38de2dbf069e5b
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 11:41:08 -0500

    betty compliance added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-int main()
+
+/**
+ * main - Entry point
+ *
+ * Description: 'betty compliant example'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-    char strArr[101];
-    strcpy(strArr, "with proper grammar, but the outcome is a piece of art,");
-    printf("%s", strArr);
-    return (0);
+	char strArr[101];
+
+	strcpy(strArr, "with proper grammar, but the outcome is a piece of art,");
+	printf("%s", strArr);
+	return (0);
 }
