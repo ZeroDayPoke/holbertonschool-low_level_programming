551e493b9e65e1a27de91a18eecabc62c540a4a2
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 13:45:07 -0500

    fix order of printfs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,10 +17,10 @@ int main(void)
 	char charVarType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charVarType));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatVarType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intVarType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longVarType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongVarType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatVarType));
 
 	return (0);
 }
