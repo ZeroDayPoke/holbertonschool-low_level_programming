ce49f90c54349b8701ebb6c00216fb460f3f91b3
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 18:43:05 -0500

    make bigger charArr

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'betty compliant example'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char charArr[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(charArr[i]);
+	}
+	putchar('\n');
+	return (0);
+}
