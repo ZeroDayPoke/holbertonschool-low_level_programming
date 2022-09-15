cdb71f9c4284e498f01f68338c01184908d040fb
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 10:16:27 -0500

    push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char strArr[101];
+    strcpy(strArr, "Programming is like building a multilingual puzzle");
+    puts(strArr);
+    return (0);
+}
