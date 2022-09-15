feef766d4e1dfe0f6b71235f0cfed5ded2ef3648
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 17:53:03 -0500

    maybe me put pieces together?

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,31 @@
-if (n < 0)
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'betty compliant example'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	printf("%i is negative\n", n);
-}
-else if (n = 0)
-{
-	printf("%i is zero\n", n);
-}
-else
-{
-	printf("%i is positive\n", n);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else if (n = 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else
+	{
+		printf("%i is positive\n", n);
+	}
+	return (0);
 }
