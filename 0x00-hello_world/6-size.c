b075e87493fb5008ae49ac4b34d508a90eabd296
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 13:41:40 -0500

    fix var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,14 +12,14 @@ int main(void)
 {
 	int intVarType;
 	float floatVarType;
-	long longVarType;
-	long long longLongVarType;
+	long int longVarType;
+	long long int longLongVarType;
 	char charVarType;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(intVarType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatVarType));
-	printf("Size of double: %zu byte(s)\n", sizeof(longVarType));
-	printf("Size of char: %zu byte(s)\n", sizeof(charVarType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charVarType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatVarType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intVarType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longVarType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongVarType));
 
 	return (0);
