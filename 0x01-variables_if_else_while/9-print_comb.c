ecac33474016df0615d56a3e35a1259bc413f873
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 19:25:52 -0500

    comma separated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'betty compliant example'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int intArr[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(intArr[i]);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
