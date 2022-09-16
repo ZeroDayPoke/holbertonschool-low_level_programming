b92d66ce6e2003ce78554ea0867d41c2f2b8bed4
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-15 19:48:17 -0500

    putcharArrXD

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'betty compliant example'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char charArr[] = "_putchar";
+
+	printf("%s\n", charArr);
+	return (0);
+}
