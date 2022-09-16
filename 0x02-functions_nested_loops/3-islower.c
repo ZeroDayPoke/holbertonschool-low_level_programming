33435d14f3d176f0610b5b03c0b997434dc65c11
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-16 11:13:09 -0500

    islower round 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - a function that determines if character is lower case
+ *
+ * Return: 1 if lowercase; 0 otherwise
+ */
+int _islower(int c)
+{
+	char cc;
+
+	for (cc = 'a'; cc <= 'z'; cc++)
+	{
+		if (cc == c)
+		{
+			return (1);
+			break;
+		}
+	}
+	return (0);
+}
