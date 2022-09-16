7c97593b4e19b208c2a67eb2a4119de125e4d04a
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-16 10:46:13 -0500

    fix doc style maybe

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * prints alphabet
+ * print_alphabet - a function that prints the alphabet
  *
- * Description: 'betty compliant example'
- *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
-
 void print_alphabet(void)
 {
 	char charArr[26] = "abcdefghijklmnopqrstuvwxyz";
