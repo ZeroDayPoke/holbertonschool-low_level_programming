7623d31f4a33109adfe80647e4809da6292062d3
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-17 10:19:10 -0500

    cant be this easy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two ints and returns sum
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+    int c;
+
+    c = a + b;
+    return (c);
+}
