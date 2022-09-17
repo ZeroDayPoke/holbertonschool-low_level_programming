8905a26e0495bf24bcd08890ca383efcce04f30f
Fluffy Elgato <elgatofluffy@gmail.com>
2022-09-17 10:42:52 -0500

    push print 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints ints in range from n to 98
+ * @n: range int input
+ * Return: 0 uwu true
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		do
+		{
+			printf("%i", n);
+			n++;
+		} while (n < 98);
+	}
+	else if (n > 98)
+	{
+		do
+		{
+			printf("%i", n);
+			n--;
+		} while (n > 98);
+	}
+	else
+	{
+		printf("%i", n);
+	}
+	printf("\n");
+}
