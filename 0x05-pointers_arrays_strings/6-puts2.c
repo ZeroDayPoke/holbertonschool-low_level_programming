fba279db1a0773c880e02a285ad9865f5d009b3f
Chris Stamper <cstampok@gmail.com>
2022-09-22 14:26:15 -0500

    fix style check next mem loc up

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@ void puts2(char *str)
 {
 	int a = 0;
 
-	while (*(str + a) != '\0')
+	while (*(str + a) != '\0' && *(str + (a + 1)) != '\0')
 	{
 		_putchar(*(str + a));
-		a+=2;
+		a += 2;
 	}
 	_putchar('\n');
 }
