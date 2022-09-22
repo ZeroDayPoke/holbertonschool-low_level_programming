180088b6b7f9492a43ef6879b608a3747b1b8206
Chris Stamper <cstampok@gmail.com>
2022-09-22 14:16:08 -0500

    maybe

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,20 @@
 void rev_string(char *s)
 {
 	int a = 0;
+	char charArr[999];
+	int b = 0;
 
 	while (*(s + a) != '\0')
 	{
+		charArr[a] = *(s + a);
 		a++;
 	}
 	a--;
 	while (a > -1)
 	{
-		*(s + a) = _putchar(*(s + a));
+		*(s + a) = charArr[b];
 		a--;
+		b++;
 	}
 	_putchar('\n');
 }
