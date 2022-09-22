1defa8ed26a6b7da75c47f1752c3bfac686e3554
Chris Stamper <cstampok@gmail.com>
2022-09-22 09:52:47 -0500

    cantblockbettysstyle

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints more numbers
+ * Return: nothing, is void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
