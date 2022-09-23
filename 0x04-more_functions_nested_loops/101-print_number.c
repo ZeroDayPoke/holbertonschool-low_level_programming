004f0a4159ada5503ab5136bfe86ab93bd46b54e
Chris Stamper <cstampok@gmail.com>
2022-09-23 17:25:33 -0500

    VICTORY!!!

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints ints
+ * @n: int to be printed
+ * Return: nothing, is void
+ */
+void print_number(int n)
+{
+	int nCount = 0;
+	int intArr[10];
+	int sign = 0;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		sign = 1;
+	}
+	while (n > 9)
+	{
+		intArr[nCount] = (n % 10);
+		nCount++;
+		n /= 10;
+	}
+	intArr[nCount] = (n % 10);
+	while (nCount >= 0)
+	{
+		if (sign == 1)
+		{
+			_putchar('-');
+			sign = 0;
+		}
+		_putchar(intArr[nCount] + '0');
+		nCount--;
+	}
+}
