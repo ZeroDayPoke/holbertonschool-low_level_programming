2a477bb5621f5ee5867cfbff92096d6cb7c86ffd
Chris Stamper <cstampok@gmail.com>
2022-09-23 18:36:53 -0500

    looks right...

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints a square
+ * Return: 0 always success
+ */
+int main(void)
+{
+	long i = 2;
+	long n = 612852475143;
+	long factor[99];
+	int factorCount = 0;
+
+	while (i <= (n / 2))
+	{
+		if (n % i == 0)
+		{
+			n = (n / i);
+			factor[factorCount] = i;
+			factorCount++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%ld\n", factor[(factorCount - 1)]);
+	return (0);
+}
