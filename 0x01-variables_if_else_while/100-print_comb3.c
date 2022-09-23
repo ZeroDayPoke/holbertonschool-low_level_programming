f58bb909206ef7db4bd4ddddf2984ff492f26a23
Chris Stamper <cstampok@gmail.com>
2022-09-23 13:22:15 -0500

    push fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,20 @@ int main(void)
 	int i, j, count;
 
 	count = 1;
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 9; i++)
 	{
 		for (j = count; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		count++;
 	}
-	putchar(8 + '0');
-	putchar(9 + '0');
 	putchar('\n');
 	return (0);
 }
