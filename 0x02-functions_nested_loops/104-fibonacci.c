4ca5ccb8b7c7f4e4e209310efd55a58064b98325
Chris Stamper <cstampok@gmail.com>
2022-09-23 11:07:53 -0500

    maybe

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 98 fib seq nums
+ * Return: 0 always success
+ */
+int main(void)
+{
+	int numCount = 0;
+	unsigned long num1, num2, num3;
+
+	printf("1, ");
+	printf("2, ");
+	num1 = 1;
+	num2 = 2;
+	while (numCount < 95)
+	{
+		num3 = num1 + num2;
+		printf("%ld, ", num3);
+		numCount++;
+		num1 = num2;
+		num2 = num3;
+	}
+	num3 = num1 + num2;
+	printf("%lu\n", num3);
+	return (0);
+}
