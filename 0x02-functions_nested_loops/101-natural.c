69c3f6094c2f57ebce321f15caf31385117f8970
Chris Stamper <cstampok@gmail.com>
2022-09-23 09:55:13 -0500

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints sum nat num to 1024
+ * 
+ * Return: 0 always success
+ */
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 5 == 0)
+		{
+			sum += i;
+		}
+		else if (i % 3 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
