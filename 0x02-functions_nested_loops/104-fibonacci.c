5fcef4b3766586d43742b323c8a31a7607f343b8
Chris Stamper <cstampok@gmail.com>
2022-09-25 13:05:50 -0500

    up to 98!!!

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int numCount = 0;
-	unsigned long num1, num2, num3;
+	unsigned long num1, num2, num3, num4, num5, num6;
 
 	printf("1, ");
 	printf("2, ");
@@ -21,7 +21,35 @@ int main(void)
 		num1 = num2;
 		num2 = num3;
 	}
+	num6 = (num3 % 10);
+	num3 /= 10;
+	num5 = (num2 % 10);
+	num2 /= 10;
+	num4 = (num1 % 10);
+	num1 /= 10;
+	while (numCount < 94)
+	{
+		num3 = num1 + num2;
+		num6 = num4 + num5;
+		if (num6 > 9)
+		{
+			num3++;
+			num6-= 10;
+		}
+		printf("%lu%lu, ", num3, num6);
+		numCount++;
+		num1 = num2;
+		num2 = num3;
+		num4 = num5;
+		num5 = num6;
+	}
 	num3 = num1 + num2;
-	printf("%lu\n", num3);
+	num6 = num4 + num5;
+	if (num6 > 9)
+	{
+		num3++;
+		num6-= 10;
+	}
+	printf("%lu%lu\n", num3, num6);
 	return (0);
 }
