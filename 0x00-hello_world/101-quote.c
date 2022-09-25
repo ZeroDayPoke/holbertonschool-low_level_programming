2066d8794e7557bff26f98049e56db3ac993d4d0
Chris Stamper <cstampok@gmail.com>
2022-09-25 11:49:01 -0500

    stylefix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,9 @@
 int main(void)
 {
 	int stringLength = 0;
-	char strArr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	stringLength = strlen(strArr);
-	write(2, strArr, stringLength);
+	char sArr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	stringLength = strlen(sArr);
+	write(2, sArr, stringLength);
 	return (1);
 }
