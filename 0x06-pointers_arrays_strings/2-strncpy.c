943d4c9515dc5f9270dcabdca8b11169034f9406
Chris Stamper <cstampok@gmail.com>
2022-09-29 08:37:18 -0500

    looks good

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copy src to dest w/ n limit
+ * @dest: where for to put string cpy
+ * @src: where string comes from
+ * @n: limit of copy
+ * Return: cpyd string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
