f0012791d464d922f09d94cc8fff874910bafb96
Chris Stamper <cstampok@gmail.com>
2022-09-29 12:05:59 -0500

    add r3k7 arr

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,30 +9,15 @@ char *leet(char *s)
 {
 	int j, i = 0;
 	char ArrS[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char r3k7[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == ArrS[j] && (j == 0 || j == 1))
+			if (s[i] == ArrS[j])
 			{
-				s[i] = '4';
-			}
-			else if (s[i] == ArrS[j] && (j == 2 || j == 3))
-			{
-				s[i] = '3';
-			}
-			else if (s[i] == ArrS[j] && (j == 4 || j == 5))
-			{
-				s[i] = '0';
-			}
-			else if (s[i] == ArrS[j] && (j == 6 || j == 7))
-			{
-				s[i] = '7';
-			}
-			else if (s[i] == ArrS[j] && (j == 8 || j == 9))
-			{
-				s[i] = '1';
+				s[i] = r3k7[j];
 			}
 		}
 		i++;
