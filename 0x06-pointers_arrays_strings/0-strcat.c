25dd8835ca1d0e9985c65eb17136e68511c2334e
Chris Stamper <cstampok@gmail.com>
2022-09-29 08:16:26 -0500

    looks right

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - meows 2 strings together
+ * @dest: where big string goes and other smol string comes
+ * @src: smol string input
+ * Return: big string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
