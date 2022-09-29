90aa244acf4c3e9923bdcbcaf4307e4a70d0aaa1
Chris Stamper <cstampok@gmail.com>
2022-09-29 11:33:23 -0500

    looks right

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - makes all words proper nouns
+ * @s: input string
+ * Return: such a proper string
+ */
+char *cap_string(char *s)
+{
+	int j, i = 0;
+	char ArrS[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+
+	while (s[i])
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i - 1] == ArrS[j] && (s[i] >= 'a' && s[i] <= 'z' ))
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
