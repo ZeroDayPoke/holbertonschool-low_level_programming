a2cecf535bb5fab565f9c54c05d89b399d690593
Chris Stamper <cstampok@gmail.com>
2022-09-29 09:24:47 -0500

    lmfao

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares strings ASCII values
+ * @s1: first string
+ * @s2: second string
+ * Return: sum of ASCII dif
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
