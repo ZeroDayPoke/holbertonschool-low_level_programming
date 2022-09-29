aa562d08f4e033cee766eb05a677f585e9d434b3
Chris Stamper <cstampok@gmail.com>
2022-09-29 15:07:30 -0500

    cannot believe this worked!

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates and points to substr if existing
+ * @haystack: string to look thru
+ * @needle: substring to locate
+ * Return: point to substr or NULL if none found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, hitCount, nedLen;
+
+	while (needle[i])
+	{
+		i++;
+	}
+	nedLen = i;
+	i = 0;
+	while (haystack[j])
+	{
+		hitCount = 0;
+		if (haystack[j] == needle[i])
+		{
+			for (i = 0; i < nedLen; i++)
+			{
+				if (haystack[j + i] == needle[i])
+				{
+					if ((hitCount + 2) > nedLen)
+					{
+						haystack += j;
+						return (haystack);
+					}
+					hitCount++;
+					continue;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		j++;
+	}
+	return (NULL);
+}
