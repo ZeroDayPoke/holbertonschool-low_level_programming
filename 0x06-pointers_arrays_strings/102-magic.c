d569ab504cf9aaaa8958294e79d761b8591cef8e
Chris Stamper <cstampok@gmail.com>
2022-10-04 13:09:13 -0500

    sus

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -18,12 +18,12 @@ int main(void)
    */
   *(p + 2) = 98;
   /* ...so that this prints 98\n */
-  printf("%p\n", &n);
-  printf("%p\n", &(a[0]));
-  printf("%p\n", &(a[1]));
-  printf("%p\n", &(a[2]));
-  printf("%p\n", &(a[3]));
-  printf("%p\n", &(a[4]));
-  printf("%p\n", &p + 2);
+  printf("add of n: %p\n", &n);
+  printf("add of a0: %p\n", &(a[0]));
+  printf("add of a1: %p\n", &(a[1]));
+  printf("add of a2: %p\n", &(a[2]));
+  printf("add of a3: %p\n", &(a[3]));
+  printf("add of a4: %p\n", &(a[4]));
+  printf("add of p + 2: %p\n", &p + 2);
   return (0);
 }
