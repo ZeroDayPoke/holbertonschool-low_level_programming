fc3d757fae35b35068c5781cabfb8b9af452fadd
Chris Stamper <cstampok@gmail.com>
2022-10-04 20:33:28 -0500

    teensified version

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,16 +11,9 @@ char *leet(char *s)
 	char ArrS[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char r3k7[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	while (s[i])
-	{
+	for (i = 0; s[i]; i++)
 		for (j = 0; j < 10; j++)
-		{
 			if (s[i] == ArrS[j])
-			{
 				s[i] = r3k7[j];
-			}
-		}
-		i++;
-	}
 	return (s);
 }
