908b00024cd4c84ab988556e2c1a1a37fe0d5ddf
Chris Stamper <cstampok@gmail.com>
2022-10-10 20:00:27 -0500

    woot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _rooter_up - used to increment root since no globals allowed
+ * @n: num to find sqrt of
+ * @intCheck: arb num used to check for sqrt
+ * Return: correct intCheck or HCF
+ */
+int _rooter_up(int n, int intCheck)
+{
+	if ((intCheck * intCheck) == n)
+	{
+		return (intCheck);
+	}
+	else if (intCheck == 0)
+	{
+		return (-1);
+	}
+	return (_rooter_up(n, intCheck - 1));
+}
+
+/**
+ * _sqrt_recursion - computes sqrt(n) if said whole int
+ * @n: num to find sqrt of
+ * Return: sqrt(n) or -1 if non-int sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	int intCheck = n;
+
+	if (n < 1)
+	{
+		return (-1);
+	}
+	return (_rooter_up(n, intCheck));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,6 +16,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _rooter_up(int n, int intCheck);
 int is_prime_number(int n);
 
 #endif
