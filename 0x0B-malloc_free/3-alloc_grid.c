00a973f739e5f78e621051dc000f1574beba9c90
Chris Stamper <cstampok@gmail.com>
2022-10-11 02:26:43 -0500

    ugh

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - points to 2D int Arr Grid initd at 0
+ * @width: width of 2D Arr
+ * @height: height of 2D Arr
+ * Return: pointer to 2D Arr or NULL if problem
+ */
+int **alloc_grid(int width, int height)
+{
+	int **intArr;
+	int i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	intArr = malloc(sizeof(int *) * height);
+	if (!(intArr))
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= height; i++)
+	{
+		intArr[i] = malloc(sizeof(int) * width);
+		for (j = 0; j <= width; j++)
+		{
+			intArr[i][j] = 0;
+		}
+	}
+	return (intArr);
+}
