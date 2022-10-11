379b7b5457acd48faa8bb35a0acb8296c806e5cc
Chris Stamper <cstampok@gmail.com>
2022-10-10 23:47:07 -0500

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints name of self
+ * @argc: int num of inbound args
+ * @argv: pointer to array of pointers w/ said args
+ * Return: Always 0 (success)... hopefully
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{
+		/* MAKE BETTY HAPPY */
+	}
+	printf("%s\n", argv[0]);
+	return (0);
+}
