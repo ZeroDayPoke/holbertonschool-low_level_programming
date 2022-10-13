8a6367ee6afc62e0c87bee4dae456510b4e1236e
Chris Stamper <cstampok@gmail.com>
2022-10-13 16:33:02 -0500

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - function that dynamically allocates mem via malloc
+ * @b: amount of mem (in bytes) to allocate
+ * Return: void pointer to location of mem allocated or exits 98 on fail
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *loca;
+
+	loca = malloc(b);
+	if (!(loca))
+	{
+		exit (98);
+	}
+	return (loca);
+}
