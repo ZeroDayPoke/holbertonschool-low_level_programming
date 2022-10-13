b7e6f1ef81a64935cde7b883005f72593ca78bf0
Chris Stamper <cstampok@gmail.com>
2022-10-13 17:43:42 -0500

    lookin good

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_calloc - function that dynamically allocates mem and inits to 0
+ * @nmemb: number of memberz in array
+ * @size: size of memberz in array
+ * Return: void pointer to location of mem allocated or NULL if bad args or malloc fail
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *loca;
+	unsigned int i;
+	char *ptr;
+
+	if ((!(nmemb)) || (!(size)) || (nmemb <= 0) || (size <= 0))
+	{
+		return (NULL);
+	}
+	loca = malloc(nmemb * size);
+	if (!(loca))
+	{
+		free(loca);
+		return (NULL);
+	}
+	ptr = loca;
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*(ptr + i) = '\0';
+	}
+	return (loca);
+}
