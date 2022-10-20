aafa31017cccd72c03da8a44c3f69d7bf62ec168
Chris Stamper <cstampok@gmail.com>
2022-10-20 17:55:41 -0500

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,6 +30,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDoge;
 
+	if ((!(name)) || (!(owner)) || (age < 0))
+	{
+		return (NULL);
+	}
 	newDoge = malloc(sizeof(dog_t));
 	if (!(newDoge))
 	{
