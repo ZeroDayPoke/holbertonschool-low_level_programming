a33110ca2163bea6438e6a0113a53a86ab4730a5
Chris Stamper <cstampok@gmail.com>
2022-10-20 12:05:49 -0500

    this easy?

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOGE_H_
+#define DOGE_H_
+
+/**
+ * struct dog - basic doge struct
+ * @name: name of doge
+ * @age: age of doge
+ * @owner: owner of doge
+ *
+ * Description: name age & owner of doge
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
