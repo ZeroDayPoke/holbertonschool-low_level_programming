00c13fe0134e3c75e0e40f2295c0922e443133e5
Chris Stamper <cstampok@gmail.com>
2022-10-20 16:42:34 -0500

    seems good

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees type dog_t doge d
+ * @d: doge to be freed from mem
+ * Return: none (void)
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
