262af9e076efcaa1333f7c29bb93d25954d899a9
Chris Stamper <cstampok@gmail.com>
2022-10-20 23:20:53 -0500

    converted to while loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,12 +17,13 @@ int (*get_op_func(char *s))(int, int)
     };
     int i;
 
-    for (i = 0; ops[i].op; i++)
+    while (ops[i].op)
     {
         if (*(ops[i]).op == *s)
         {
             return (ops[i].f);
         }
+        i++;
     }
     return (NULL);
 }
