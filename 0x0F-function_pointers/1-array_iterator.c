8c7f5402513b4985cbb5de3c65bd8a391f9c137a
Chris Stamper <cstampok@gmail.com>
2022-10-20 19:22:34 -0500

    add prot for various edge cases

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function that invokes function that operates on array int mems
+ * array_iterator - fun that invokes function that operates on array int mems
  * @array: target int array
  * @size: size of int array
  * @action: pointer to function to invoke
@@ -11,6 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (!(array) || size < 1 || !(action))
+	{
+		exit(EXIT_FAILURE);
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(*(array + i));
