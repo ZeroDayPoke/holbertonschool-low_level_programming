599097b3f193332cf6742fb401f065aad98482e0
Chris Stamper <cstampok@gmail.com>
2022-10-20 21:52:02 -0500

    update 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * main - prints specified operation result to SO
+ * @argc: num of args
+ * @argv: array of pointers to args
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2] != ('+' || '-' || '*' || '/' || '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2] == ('/' || '%')) && (argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), (argv[3])));
+	return (0);
+}
