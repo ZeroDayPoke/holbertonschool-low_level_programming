2b1a4a7ab65266adf4b440320f432299232d6d15
Chris Stamper <cstampok@gmail.com>
2022-10-21 14:41:54 -0500

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,10 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * print_all
-
+ * print_all - prints variadic variable arg list
+ * @format: total dynamic strings in... named format
  */
 void print_all(const char * const format, ...)
 {
+	unsigned int i = 0, j;
+	va_list varArgList;
+	char fArr[] = {"cifs"};
 
+	va_start(varArgList, format);
+	while (format[i])
+	{
+		j = 0;
+		while (format[i] != fArr[j])
+		{
+			printf("%s, ", va_arg(varArgList, char *));
+			j++;
+		}
+		i++;
+	}
+	va_end(varArgList);
+	printf("\n");
 }
