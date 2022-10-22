4892da3e23d2b139fec5f362f6f50eb32b1794af
Chris Stamper <cstampok@gmail.com>
2022-10-21 22:54:48 -0500

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,9 +40,10 @@ void p_all_str(va_list strArg)
 	char *charArr;
 
 	charArr = va_arg(strArg, char *);
-	if (!(charArr))
+	if (!(*charArr))
 	{
 		printf("(nil)");
+		return;
 	}
 	printf("%s", charArr);
 }
@@ -61,7 +62,7 @@ void print_all(const char * const format, ...)
 	bypass betty[] = {{p_all_char}, {p_all_int}, {p_all_float}, {p_all_str}};
 
 	va_start(varArg, format);
-	while (format[i])
+	while (format[i] && format)
 	{
 		j = 0;
 		while ((Arr[j]) && (format[i] != Arr[j]))
