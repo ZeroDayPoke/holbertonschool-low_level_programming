74be4d0b149be6a556e6dcbc7830fed1b398ce8a
Chris Stamper <cstampok@gmail.com>
2022-10-21 22:26:50 -0500

    YASSSS

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,26 +1,78 @@
 #include "variadic_functions.h"
 
 /**
+ * p_all_char - prints chars from print_all
+ * @chrArg: inbound chrArg
+ * Return: just prints to SO
+ */
+void p_all_char(va_list chrArg)
+{
+	printf("%c, ", va_arg(chrArg, int));
+}
+
+/**
+ * p_all_int - prints ints from print_all
+ * @intArg: inbound intArg
+ * Return: just prints to SO
+ */
+void p_all_int(va_list intArg)
+{
+	printf("%d, ", va_arg(intArg, int));
+}
+
+/**
+ * p_all_float - prints floats from print_all
+ * @fltArg: inbound float
+ * Return: just prints to SO
+ */
+void p_all_float(va_list fltArg)
+{
+	printf("%f, ", va_arg(fltArg, double));
+}
+
+/**
+ * p_all_str - prints strings from print_all
+ * @strArg: inbound strArg
+ * Return: just prints to SO
+ */
+void p_all_str(va_list strArg)
+{
+	char *charArr;
+
+	charArr = va_arg(strArg, char *);
+	if (!(charArr))
+	{
+		printf("(nil), ");
+	}
+	printf("%s, ", charArr);
+}
+
+/**
  * print_all - prints variadic variable arg list
- * @format: total dynamic strings in... named format
+ * @format: input arg strs format
+ * Return: void (none)
  */
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0, j;
-	va_list varArgList;
-	char fArr[] = {"cifs"};
+	va_list varArg;
+	char Arr[] = "cifs";
+	bypass betty[] = {{p_all_char}, {p_all_int}, {p_all_float}, {p_all_str}};
 
-	va_start(varArgList, format);
+	va_start(varArg, format);
 	while (format[i])
 	{
 		j = 0;
-		while (format[i] != fArr[j])
+		while ((Arr[j]) && (format[i] != Arr[j]))
 		{
-			printf("%s, ", va_arg(varArgList, char *));
 			j++;
+		}
+		if (Arr[j])
+		{
+			betty[j].fun(varArg);
 		}
 		i++;
 	}
-	va_end(varArgList);
+	va_end(varArg);
 	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,6 +10,18 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * struct bypass_if_limit - struct to bypass if limit on 3
+ * @fun: pointer to fun to print associated type
+ *
+ * Description: ... really tho? almost seems intentional
+ */
+struct bypass_if_limit
+{
+	void (*fun)(va_list varArg);
+};
+typedef struct bypass_if_limit bypass;
+
 /*
  * prototypes - function protos below
  */
@@ -17,5 +29,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void p_all_char(va_list chrArg);
+void p_all_int(va_list intArg);
+void p_all_float(va_list fltArg);
+void p_all_str(va_list strArg);
 
 #endif
