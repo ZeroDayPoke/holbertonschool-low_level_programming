6271e96456e1aed89e0620e8e4280ee7897cd77a
Chris Stamper <cstampok@gmail.com>
2022-11-04 08:40:44 -0500

    add null head handling cond

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint - deploys self destructing vel'koz on sll
+ * free_listint2 - deploys self destructing vel'koz on sll
  * @head: sll HEAD pointer
  * Return: vel'koz
  */
@@ -9,6 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *targetCycler;
 
+	if (!(head))
+	{
+		return;
+	}
 	while (*head)
 	{
 		targetCycler = (*head)->next;
