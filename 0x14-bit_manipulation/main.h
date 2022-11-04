9444f80bde4b1158fd6ab05727f2f1a535724cc4
Chris Stamper <cstampok@gmail.com>
2022-11-03 22:29:18 -0500

    init

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+/*
+ * libraries - to be included
+ */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/*
+ * prototypes - function protos below
+ */
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
