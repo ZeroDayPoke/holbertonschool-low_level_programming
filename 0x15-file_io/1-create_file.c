71d8679a2e49675f02d2f186ada3cfc45397e835
Chris Stamper <cstampok@gmail.com>
2022-11-06 18:53:10 -0600

    add txtcontent guard

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	while (text_content[txtLen])
+	if (text_content)
 	{
-		txtLen++;
+		while (text_content[txtLen])
+		{
+			txtLen++;
+		}
 	}
 	oNum = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wNum = write(oNum, text_content, txtLen);
