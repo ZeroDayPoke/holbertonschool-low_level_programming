2b349f12f9ad330786ace3b9b473193389e2d5f8
Chris Stamper <cstampok@gmail.com>
2022-11-06 18:45:35 -0600

    strlen fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		txtLen++;
 	}
-	txtLen--;
+	txtLen++;
 	oNum = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
 	wNum = write(oNum, text_content, txtLen);
 	if (oNum < 0 || wNum < 0)
