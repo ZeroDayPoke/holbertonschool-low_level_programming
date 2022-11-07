4c13f79ea39a6b6f60e76a1e72d4a23f536f4fe5
Chris Stamper <cstampok@gmail.com>
2022-11-06 18:51:31 -0600

    remove exec file perm oof

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		txtLen++;
 	}
-	oNum = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
+	oNum = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wNum = write(oNum, text_content, txtLen);
 	if (oNum < 0 || wNum < 0)
 	{
