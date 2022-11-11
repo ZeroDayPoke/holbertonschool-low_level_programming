29dde73e28b708d1da79e43031a4ae897d60eb0b
Chris Stamper <cstampok@gmail.com>
2022-11-11 16:31:20 -0600

    add existing key handling cond

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,14 +3,14 @@
 /**
  * hash_table_set - function that adds ele to htable
  * @ht: htable to add kv to
- * @key: key to be added
+ * @key: key to be added OR HAVE VALUE UPDATED
  * @value: value paired w key
  * Return: 1 on success 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode;
-	unsigned long int hashKey, idx;
+	unsigned long int hashKey, idx, i;
 
 	if (!(ht) || !(key) || !(value) || (!(key[0])))
 	{
@@ -23,6 +23,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	hashKey = hash_djb2((const unsigned char *)key);
 	idx = hashKey % ht->size;
+	/* check if key already exists and if so update its value pair */
+	for (i = 0; ht->array[i]; i++)
+	{
+		if (!(strcmp(key, ht->array[i]->key)))
+		{
+			free(newNode);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+	}
+	/* populate new node and make first in array */
 	newNode->key = strdup(key);
 	newNode->value = strdup(value);
 	newNode->next = ht->array[idx];
