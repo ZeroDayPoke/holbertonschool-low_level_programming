a088a4136091f44bd693041358905e7c46d26b17
Chris Stamper <cstampok@gmail.com>
2022-11-11 17:36:22 -0600

    looks goodish

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_deletes - deletes hash_table_t ht
+ * @ht: htable to delete
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *scrubber1, *scrubber2;
+
+	if (!(ht))
+	{
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			scrubber1 = ht->array[i];
+			while (scrubber1)
+			{
+				scrubber2 = scrubber1->next;
+				free(scrubber1->value);
+				free(scrubber1->key);
+				free(scrubber1);
+				scrubber1 = scrubber2;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
