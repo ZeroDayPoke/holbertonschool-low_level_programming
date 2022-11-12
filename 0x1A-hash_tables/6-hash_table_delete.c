274408a3ec939d065da6b42b35530f85261f7ed6
Chris Stamper <cstampok@gmail.com>
2022-11-12 13:43:05 -0600

    big sadge

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,20 +10,26 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *scrubber, *loader;
 
-	if (!ht)
+	if (!(ht))
 	{
+		return;
+	}
+	else if (!(ht->array) || ht->size <= 0)
+	{
+		free(ht);
 		return;
 	}
 	for (i = 0; i < ht->size; i++)
 	{
 		loader = ht->array[i];
-		while (loader != NULL)
+		while (loader)
 		{
-			scrubber = loader;
-			loader = scrubber->next;
-			free(scrubber->value);
-			free(scrubber->key);
-			free(scrubber);
+			scrubber = loader->next;
+			if (loader->value)
+				free(loader->value);
+			free(loader->key);
+			free(loader);
+			loader = scrubber;
 		}
 	}
 	free(ht->array);
