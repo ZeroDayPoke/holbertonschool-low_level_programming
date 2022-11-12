81a05885b9500450399ba86b6f4d16de2a65f1cf
Chris Stamper <cstampok@gmail.com>
2022-11-11 18:59:05 -0600

    freedom...

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -27,9 +27,9 @@ void hash_table_delete(hash_table_t *ht)
 				free(scrubber1);
 				scrubber1 = scrubber2;
 			}
-			free(ht->array[i]);
 		}
 	}
+	free(ht->array[i]);
 	free(ht->array);
 	free(ht);
 }
