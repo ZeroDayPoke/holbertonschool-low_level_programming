a63d6918463d41c441a7f4ff6ca2d4ed0909908b
Chris Stamper <cstampok@gmail.com>
2022-11-12 12:50:49 -0600

    test

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *scrubber1, *scrubber2;
+	hash_node_t *scrubber;
 
 	if (!(ht))
 	{
@@ -23,18 +23,14 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		if (ht->array[i])
 		{
-			scrubber1 = ht->array[i];
-			while (scrubber1)
+			while (ht->array[i])
 			{
-				scrubber2 = scrubber1->next;
-				free(scrubber1->value);
-				free(scrubber1->key);
-				free(scrubber1);
-				scrubber1 = scrubber2;
+				scrubber = ht->array[i]->next;
+				free(ht->array[i]->value);
+				free(ht->array[i]->key);
+				free(ht->array[i]);
+				ht->array[i] = scrubber;
 			}
-		}
-		else
-		{
 			free(ht->array[i]);
 		}
 	}
