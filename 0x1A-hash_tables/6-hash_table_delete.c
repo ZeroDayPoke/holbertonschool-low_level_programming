e2157916f2c6c2f72f57a2e2824271b92147410f
Chris Stamper <cstampok@gmail.com>
2022-11-12 13:34:53 -0600

    remove else if cond hand

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -14,11 +14,6 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		return;
 	}
-	else if (!(ht->array) || ht->size <= 0)
-	{
-		free(ht);
-		return;
-	}
 	for (i = 0; i < ht->size; i++)
 	{
 		loader = ht->array[i];
