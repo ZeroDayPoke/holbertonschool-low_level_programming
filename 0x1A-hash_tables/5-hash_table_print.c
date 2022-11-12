e973ecdf8e37a8d7412aae82bbb02896fdb8814b
Chris Stamper <cstampok@gmail.com>
2022-11-11 18:22:33 -0600

    removed extra space char

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 			}
 			stupidcommas = 1;
-			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			ht->array[i] = ht->array[i]->next;
 		}
 	}
