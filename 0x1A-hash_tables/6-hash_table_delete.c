870dc57a2e620e4c3e935fc10c646ac57c67d90d
Chris Stamper <cstampok@gmail.com>
2022-11-12 13:25:54 -0600

    test

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -22,7 +22,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		loader = ht->array[i];
-		while (loader)
+		while (loader != NULL)
 		{
 			scrubber = loader->next;
 			if (loader->value)
