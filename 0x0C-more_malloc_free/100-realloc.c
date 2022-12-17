04fd8e86325086600428eff1c8e1c0ad9392917a
Chris Stamper <cstampok@gmail.com>
2022-12-17 14:58:34 -0600

    possible realloc solution

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates mem block
+ * @ptr: prev allocd block
+ * @old_size: byte size of old block
+ * @new_size: byte size of new block
+ * Return: pointer to new block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int i, min_size;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	min_size = old_size < new_size ? old_size : new_size;
+	for (i = 0; i < min_size; i++)
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (new_ptr);
+}
