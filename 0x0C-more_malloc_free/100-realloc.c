4b2b88a572375085d2812a5db7de6650aee8ae02
Chris Stamper <cstampok@gmail.com>
2022-12-17 15:09:53 -0600

    test

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,14 +12,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new_ptr;
 	unsigned int i, min_size;
 
-	if (new_size == 0)
-	{
-		if (ptr)
-			free(ptr);
-		return (NULL);
-	}
 	if (ptr == NULL)
 		return (malloc(new_size));
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
