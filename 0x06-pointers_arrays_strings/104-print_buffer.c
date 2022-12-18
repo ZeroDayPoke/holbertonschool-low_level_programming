23f917de201d95a28a7fcef7f17ce210070f9699
Chris Stamper <cstampok@gmail.com>
2022-12-17 18:59:48 -0600

    betty fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,38 +1,44 @@
 #include "holberton.h"
 
+/**
+ * print_buffer - prints b to SO
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
 	int i, j, remaining;
 
-    if (size <= 0)
-    {
-        putchar('\n');
-        return;
-    }
-    for (i = 0; i < size; i++)
-    {
-        if (i % 10 == 0)
-            printf("%.8x: ", i);
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (i % 10 == 0)
+			printf("%.8x: ", i);
 		if (i % 2 == 0)
 			printf("%.2x%.2x ", b[i], b[i + 1]);
-        if (i % 10 == 9)
-        {
-            for (j = i - 9; j <= i; j++)
-                putchar(isprint(b[j]) ? b[j] : '.');
-            putchar('\n');
-        }
-    }
+		if (i % 10 == 9)
+		{
+			for (j = i - 9; j <= i; j++)
+				putchar(isprint(b[j]) ? b[j] : '.');
+			putchar('\n');
+		}
+	}
 	remaining = size % 10;
-    if (remaining > 0)
-    {
-        for (i = 0; i < 10 - remaining; i++)
-            printf("  ");
+	if (remaining > 0)
+	{
+		for (i = 0; i < 10 - remaining; i++)
+			printf("  ");
 		if (b[i + 1])
-        	printf("  ");
+			printf("  ");
 		else
 			putchar(' ');
-        for (i = size - remaining; i < size; i++)
-            putchar(isprint(b[i]) ? b[i] : '.');
-        putchar('\n');
-    }
+		for (i = size - remaining; i < size; i++)
+			putchar(isprint(b[i]) ? b[i] : '.');
+		putchar('\n');
+	}
 }
