8029feb891161eac9403d1fce73c1f0b7e308da6
Chris Stamper <cstampok@gmail.com>
2022-12-17 18:09:58 -0600

    fix betty

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * infinite add - adds redonk large nums
+ * infinite_add - adds redonk large nums
  * @n1: first number (str form)
  * @n2: second number (str form)
  * @r: buffer
@@ -10,7 +10,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0, j = 0, k, carry = 0, sum;
+	int i = 0, j = 0, k, carry = 0, sum;
 
 	k = (size_r - 1);
 	while (n1[i + 1])
@@ -18,20 +18,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (n2[j + 1])
 		j++;
 	r[size_r] = '\0';
-    while (i >= 0 || j >= 0 || carry > 0)
-    {
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
-        r[k] = (sum % 10) + '0';
-        carry = sum / 10;
-        i--;
-        j--;
-        k--;
-    }
-    if (k < 0)
-        return (0);
-    return (r + k + 1);
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+		r[k] = (sum % 10) + '0';
+		carry = sum / 10;
+		i--;
+		j--;
+		k--;
+	}
+	if (k < 0)
+		return (0);
+	return (r + k + 1);
 }
