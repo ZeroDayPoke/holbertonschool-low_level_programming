d08fd6eb692be548cc6e2d422ef76dd3dc680b20
Chris Stamper <cstampok@gmail.com>
2022-12-18 16:28:51 -0600

    fix maybe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,26 +2,75 @@
 #include "lists.h"
 
 /**
+ * numNodes - helps print safely by comp num nodes in loop
+ * @head: sll HEAD pointer
+ * Return: num of nodes in circular or 0
+ */
+size_t numNodes(const listint_t *head)
+{
+	const listint_t *nodeA, *nodeB;
+	size_t count = 1;
+
+	if (!head)
+		return (0);
+	if (!head->next)
+		return (0);
+	nodeA = head->next;
+	nodeB = head->next->next;
+	while (nodeB)
+	{
+		if (nodeA == nodeB)
+		{
+			nodeA = head;
+			while (nodeA != nodeB)
+			{
+				count++;
+				nodeA = nodeA->next;
+				nodeB = nodeB->next;
+			}
+			nodeA = nodeA->next;
+			while (nodeA != nodeB)
+			{
+				count++;
+				nodeA = nodeA->next;
+			}
+			return (count);
+		}
+		nodeA = nodeA->next;
+		nodeB = nodeB->next->next;
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - prints SLL listint_t list
  * @head: sll HEAD pointer
  * Return: num of nodes in list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
-	size_t count = 0;
+	const listint_t *nodeA;
+	size_t count = 0, i = 0;
 
-	current = head;
-	while (current)
+	count = numNodes(head);
+	nodeA = head;
+	if (count == 0)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
-		if (current <= head)
+		while (nodeA)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98);
+			count++;
+			printf("[%p] %d\n", (void *)nodeA, nodeA->n);
+			nodeA = nodeA->next;
 		}
+	}
+	else
+	{
+		for (i = 0; i < count; i++)
+		{
+			printf("[%p] %d\n", (void *)nodeA, nodeA->n);
+			nodeA = nodeA->next;
+		}
+		printf("-> [%p] %d\n", (void *)nodeA, nodeA->n);
 	}
 	return (count);
 }
